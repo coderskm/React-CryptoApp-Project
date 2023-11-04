dec623e665c6ff32d78c10f8d15d886f68d02c06
Sumit Kumar Mishra <sumitkumarmishra.code@gmail.com>
2023-11-04 18:22:48 +0530

    refactored cryptorjs app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Coincard from "./components/Coincard";
+
 function App() {
   const [coinsList, setCoinsList] = useState([]);
   const [name, setName] = useState("");
+
+  const url = import.meta.env.VITE_API_URL;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      "X-API-KEY": import.meta.env.VITE_API_KEY,
+    },
+  };
+
   useEffect(() => {
-    fetch(`https://api.coinstats.app/public/v1/coins`)
+    fetch(url, options)
       .then((response) => response.json())
-      .then((data) => setCoinsList(data.coins));
+      .then((data) => setCoinsList(data.result))
+      .catch((err) => console.log(err));
   });
-  const filteredCoins = coinsList.filter((coin) => coin.name.toLowerCase().includes(name.trim().toLowerCase()));
+
+  const filteredCoins = coinsList?.filter((coin) => {
+    return coin.name.toLowerCase().includes(name.trim().toLowerCase());
+  });
+
   return (
     <>
       <div className="crypto-header">
@@ -19,12 +35,13 @@ function App() {
           placeholder="enter name of coin to search.."
           onChange={(e) => setName(e.target.value)}
         />
+        
       </div>
       <div className="crypto-display">
         {filteredCoins.length == 0 ? (
           <p>No Coin Found :(</p>
         ) : (
-          filteredCoins.map((coin) => <Coincard key={coin.id} coin={coin} />)
+          filteredCoins?.map((coin) => <Coincard key={coin.id} coin={coin} />)
         )}
       </div>
     </>
